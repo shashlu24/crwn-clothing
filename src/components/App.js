d35bed70c25a7caa9b781dc7eab92fdf1b93f888
unknown <singhshashank32@gmail.com>
2021-01-09 14:04:24 +0530

    created project with create react app and added files for homepage

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "../css/App.css";
+import Homepage from "./Homepage";
+
+function App() {
+  return (
+    <div className="App">
+      <Homepage />
+    </div>
+  );
+}
+
+export default App;
