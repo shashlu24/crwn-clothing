c1b8a700c04defd3babfceaae58cc72ddfe7ca49
unknown <singhshashank32@gmail.com>
2021-01-10 00:29:58 +0530

    created a Header Component

--- src/App.js
+++ src/App.js
@@ -3,20 +3,15 @@ import "./App.css";
 import Homepage from "./pages/Homepage";
 import { Switch, Route } from "react-router-dom";
 import ShopPage from "./pages/shop/ShopPage";
-
-const HatsPage = () => {
-  return <div>Hats Page</div>;
-};
+import Header from "./components/header/Header";
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Switch>
         <Route exact path="/">
           <Homepage />
-        </Route>
-        <Route exact path="/shop/hats">
-          <HatsPage />
         </Route>
         <Route exact path="/shop">
           <ShopPage />
